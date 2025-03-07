f5e3d23324accf4ed90e87c0de44d7425afb9e83
Andrew Wright <andrew@outerlands.co>
2025-03-07 07:30:04 +0000

    chore: remove photorealistic map

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,14 @@
 import { useState } from "react";
 
 // Maps
-import PhotorealisticMap from "./maps/PhotorealisticMap";
 import ThreeDTilesMap from "./maps/ThreeDTilesMap";
 
 const App = () => {
-  const [mapType, setMapType] = useState("3d");
-
   return (
     <>
-      <p>Select the type of map you'd like to use</p>
+      <p>Pubs in the Sun 2</p>
 
-      <button onClick={() => setMapType("basic")}>Basic</button>
-      <button onClick={() => setMapType("3d")}>3D Tiles Map</button>
-
-      {mapType === "basic" && <PhotorealisticMap />}
-
-      {mapType === "3d" && <ThreeDTilesMap />}
+      <ThreeDTilesMap />
     </>
   );
 };
