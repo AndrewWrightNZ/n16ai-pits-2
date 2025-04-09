09f8d32b69d3ff90f6940faf74ce6d93fb617878
Andrew Wright <andrew@outerlands.co>
2025-04-09 14:38:06 +0100

    feat: render pubs on map

---
+++ src/pages/finder/_shared/components/renderPubsOfType.tsx
@@ -0,0 +1,52 @@
+// Hooks
+
+// Types
+
+// Components
+import { Pub } from "../../../../_shared/types";
+import usePubs from "../hooks/usePubs";
+import CustomMarker from "./customMarker";
+
+interface RenderPubsOfTypeProps {
+  filterName: string;
+}
+
+const RenderPubsOfType = ({ filterName }: RenderPubsOfTypeProps) => {
+  //
+  // Hooks
+  const {
+    data: {
+      pubsInTheSun = [],
+      pubsPartiallyInTheSun = [],
+      pubsNotInTheSun = [],
+    },
+  } = usePubs();
+
+  //
+  // Variables
+  let pubsToRender: Pub[] = [];
+
+  switch (filterName) {
+    case "full_sun":
+      pubsToRender = pubsInTheSun;
+      break;
+    case "partial_sun":
+      pubsToRender = pubsPartiallyInTheSun;
+      break;
+    case "no_sun":
+      pubsToRender = pubsNotInTheSun;
+      break;
+    default:
+      pubsToRender = [];
+  }
+
+  return (
+    <>
+      {pubsToRender?.map((pub) => (
+        <CustomMarker key={pub.id} pubDetails={pub} filterName={filterName} />
+      ))}
+    </>
+  );
+};
+
+export default RenderPubsOfType;
